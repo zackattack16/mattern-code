16f5e821c53559b52bd8229d0ac6dc1a6d33efd2
Simon Hajduk <simonhajduk@icloud.com>
2024-01-25 10:54:53 -0800

    added row creation functionality

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const xImput = document.getElementById("axisX");
-const yImput = document.getElementById("axisY");
-const chose = document.getElementById("colorChose");
-const butten1 = document.getElementById("butten1");
+const xInput = document.getElementById("axisX");
+const yInput = document.getElementById("axisY");
+const choose = document.getElementById("colorChoose");
+const button1 = document.getElementById("button1");
 const group = document.getElementById("group");
-butten1.addEventListener("click", startpatter);
 
-function startpatter() {
-    const div = document.createElement('div');
-    div.style.backgroundColor = "red";
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.left = "50px";
-    div.style.top = "50px";
-    //for (let i = 0; i < xImput; i++){
-        
-    //} 
-    group.appendChild(div).repeat(xImput);
+button1.addEventListener("click", startpattern);
+
+function startpattern() {
+
+    let div;
+
+    for (let i = 0; i < xInput.value; i++){
+        div = document.createElement('div');
+    
+        div.style.backgroundColor = "yellow";
+        div.style.width = "30px";
+        div.style.height = "30px";
+        div.style.border = "10px solid white";
+
+
+
+        div.id = "row";
+        group.appendChild(div);
+    } 
+    
 }
