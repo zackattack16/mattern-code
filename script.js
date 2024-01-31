ee04b9dbb18455686ddceeceb07d80263b28cbe7
Simon Hajduk <simonhajduk@icloud.com>
2024-01-31 09:33:18 -0800

    Finally Working 😀

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const yInput = document.getElementById("axisY");
 const choose = document.getElementById("colorChoose");
 const button1 = document.getElementById("button1");
 const group = document.getElementById("group");
-
+const test = document.getElementById("test");
 button1.addEventListener("click", startpattern);
 
 function startpattern() {
@@ -17,10 +17,10 @@ function startpattern() {
         div = document.createElement('div');
     
         
-   
+        
         
 
-        for (let i = 0; i < yInput.value; i++){
+        for (let j = 0; j < yInput.value; j++){
             div2 = document.createElement('div');
         
             div2.style.backgroundColor = choose.value;
@@ -29,16 +29,18 @@ function startpattern() {
             div2.style.border = "10px solid white";
     
     
-    
-            div2.id = "column";
+            div2.id = ("id: "+ String(j + 1));
+            div2.classList.add("point");
+            
             div.appendChild(div2);
+            div2.addEventListener('click', function() {
+                this.style.backgroundColor = choose.value;
+            });
+            
         } 
-
-        div.id = "row";
+        div.id = ("id: "+ String(i + 1));
+        div.classList.add("row");
         group.appendChild(div);
         
     } 
-    
-    
-    
 }
