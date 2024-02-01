a0e6621cdb00441274e0d1583f1d75c0aa4fcfc0
Simon Hajduk <simonhajduk@icloud.com>
2024-01-31 17:34:44 -0800

    Added a confetti generator

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const choose = document.getElementById("colorChoose");
 const button1 = document.getElementById("button1");
 const group = document.getElementById("group");
 const test = document.getElementById("test");
+const button2 = document.getElementById("buttones");
 button1.addEventListener("click", startpattern);
+let confetterInterval;
 
+button2.addEventListener("mousedown", function() {
+    confetterInterval = setInterval(confetter, 1); // Run confetter every 100ms
+});
+
+button2.addEventListener("mouseup", function() {
+    clearInterval(confetterInterval); // Stop running confetter
+});
 
 var log = 0;
  // let log = new Text('?');
-
+var idgen = 1;
 function logButtons(e) {
   log = `${e.buttons}`; // log.nodeValue= `${e.buttons} (${e.type})`;
 }
@@ -20,7 +29,15 @@ document.addEventListener("mousedown", logButtons);
 
 
 
-
+function confetter() {
+    let min = 1; // minimum value
+    
+    let bar = Math.floor(Math.random() * ((yInput.value*xInput.value)));
+    console.log(bar);
+    const rowelement = document.getElementById("id" + bar);
+    console.log(rowelement);
+    rowelement.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
 function startpattern() {
 
     let div;
@@ -44,7 +61,8 @@ function startpattern() {
             div2.style.border = "0px solid dimgrey";
     
     
-            div2.id = ("id: "+ String(j + 1));
+            div2.id = ("id" + String(idgen));
+            idgen++;
             div2.classList.add("point");
             
             div.appendChild(div2);
@@ -58,7 +76,7 @@ function startpattern() {
             });
             
         } 
-        div.id = ("id: "+ String(i + 1));
+        
         div.classList.add("row");
         group.appendChild(div);
         
