a816ffd3e1454e79be4497c09571e0c7265fe991
Simon Hajduk <simonhajduk@icloud.com>
2024-02-02 13:15:21 -0800

    Co-authored-by: zackattack16 <zackattack16@users.noreply.github.com>
    added a flat brush, a square/circle selector, and a point size selector

--- script.js
+++ script.js
@@ -4,42 +4,50 @@ const choose = document.getElementById("colorChoose");
 const button1 = document.getElementById("button1");
 const group = document.getElementById("group");
 const test = document.getElementById("test");
-const button2 = document.getElementById("buttones");
+const button2 = document.getElementById("confetti");
+const size = document.getElementById("size");
+const shapes = document.getElementById("shapes")
+const brushselector = document.getElementById("brushselector");
+
 button1.addEventListener("click", startpattern);
 let confetterInterval;
 
+
 button2.addEventListener("mousedown", function() {
-    confetterInterval = setInterval(confetter, 1); // Run confetter every 100ms
+    confetterInterval = setInterval(confetter, 1); 
 });
 
-button2.addEventListener("mouseup", function() {
-    clearInterval(confetterInterval); // Stop running confetter
+document.addEventListener("mouseup", function() {
+    clearInterval(confetterInterval); 
 });
 
 var log = 0;
- // let log = new Text('?');
+
 var idgen = 1;
 function logButtons(e) {
-  log = `${e.buttons}`; // log.nodeValue= `${e.buttons} (${e.type})`;
+  log = `${e.buttons}`;
 }
 
 document.addEventListener("mouseup", logButtons);
 document.addEventListener("mousedown", logButtons);
-// document.addEventListener('mousemove', logButtons);
+
 
 
 
 function confetter() {
-    let min = 1; // minimum value
-    
+    let min = 1; 
+    const xInput = document.getElementById("axisX");
+    const yInput = document.getElementById("axisY");
+
     let bar = Math.floor(Math.random() * ((yInput.value*xInput.value)));
-    console.log(bar);
+    console.log("Bar: " + bar);
     const rowelement = document.getElementById("id" + bar);
     console.log(rowelement);
+
     rowelement.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 }
 function startpattern() {
-
+    idgen = 0;
     let div;
     let div2;
     document.getElementById("group").innerHTML = "";
@@ -47,29 +55,52 @@ function startpattern() {
 
     for (let i = 0; i < xInput.value; i++){
         div = document.createElement('div');
-    
+        
         
         
         
 
         for (let j = 0; j < yInput.value; j++){
             div2 = document.createElement('div');
-        
+            const shapes = document.getElementById("shapes");
             div2.style.backgroundColor = choose.value;
             div2.style.width = "5px";
             div2.style.height = "5px";
             div2.style.border = "0px solid dimgrey";
-    
+            
     
             div2.id = ("id" + String(idgen));
             idgen++;
             div2.classList.add("point");
-            
+            div2.style.width = (size.value+ "px");
+            div2.style.height = (size.value+ "px");
+            if (shapes.value == "circles") {
+                div2.style.borderRadius = "20px";
+            }
             div.appendChild(div2);
             
             div2.addEventListener('mouseenter', function() {
                     if (log == "1") {
-                        this.style.backgroundColor = choose.value;
+                        
+                        if (brushselector.value == "point") {
+                            
+                            this.style.backgroundColor = choose.value;
+                            startid = this.id.slice(2);
+                            console.log("startid:" +startid);
+                        }
+                        else if (brushselector.value == "flat") {
+                            
+                            startid = (this.id.slice(2)-2);
+
+                            console.log("startid:" +startid);
+                            
+                            this.style.backgroundColor = choose.value;
+                            document.getElementById("id"+startid).style.backgroundColor = choose.value;
+                            document.getElementById("id"+(startid+1)).style.backgroundColor = choose.value;
+                            document.getElementById("id"+(startid+3)).style.backgroundColor = choose.value;
+                            document.getElementById("id"+(startid+4)).style.backgroundColor = choose.value;
+                        }
+                        
                     }
                     
                 
